68f3c13a0d90af3bb899888c6434097ea83ffb0b
jun-yunan <junyunan341695@gmail.com>
2023-03-28 19:06:35 +0700

    first push

---
+++ js_nak/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('.itemOrder').hide();
+    $('.cateOrder').click(function (e) { 
+        e.preventDefault();
+        $(this).next().slideDown()
+    });
+
+    $('.itemOrder').mouseleave(function () { 
+        $(this).slideUp();
+    });
+    console.log("Hello ae");
+});
